85ad1dca0f7cd93d60042c09003734cccfc254f3
Adermititis <30134007@nescol.ac.uk>
2023-09-06 11:49:38 +0100

    .

---
+++ src/Display_Haiku.java
@@ -0,0 +1,9 @@
+
+public class Display_Haiku {
+    public static void main(String[] args) {
+        System.out.println("Breaking headlines flash,");
+        System.out.println("World's chaos unveiled in print,");
+        System.out.println("Seek peace 'midst the storm.");
+        }
+}
+
---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //writes out Hello world to the console
+    }
+}
